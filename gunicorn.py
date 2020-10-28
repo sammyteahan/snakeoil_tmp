0308776ed6e0a01c8929899424a40f2d5aa22551
sammyteahan <sammyteahan@gmail.com>
2020-10-28 14:29:15 -0600

    remove template nesting

--- gunicorn.py
+++ gunicorn.py
@@ -1,6 +1,5 @@
 site_name = 'snakeoil'
 
-# command = '/srv/{}/venv/bin/gunicorn'.format(site_name)
 command = '/home/ubuntu/.local/bin/gunicorn'
 python = '/srv/{}'.format(site_name)
 bind = '127.0.0.1:8001'
